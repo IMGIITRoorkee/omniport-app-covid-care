753421ceacf73b8d4ddbc92e22a54186549f24a3
Rochak Jain <rochak_r@ec.iitr.ac.in>
2021-05-03 02:21:10 +0530

    Add serializers for PlasmaDonor and Resource Model

---
+++ serializers/donor.py
@@ -0,0 +1,44 @@
+from formula_one.serializers.base import ModelSerializer
+
+from Covid_Care.models import PlasmaDonor
+from Covid_Care.serializers import RequestsSerializer
+
+
+class PlasmaDonorSerializer(ModelSerializer):
+    """
+    Details about the serializer
+    """
+
+    request = RequestsSerializer(
+        fields=[
+            'pk',
+            'patient_name',
+            'contact',
+            'pin_code',
+            'address'
+        ]
+    )
+
+    class Meta:
+        model = PlasmaDonor
+        fields = [
+            'pk',
+            'name',
+            'contact',
+            'blood_group',
+            'tested_positive',
+            'positive_when',
+            'vaccinated',
+            'pin_code',
+            'address',
+            'request',
+            'other_contact',
+        ]
+        read_only = [
+            'name',
+            'contact',
+            'blood_group',
+            'tested_positive',
+            'pin_code',
+            'address',
+        ] 
---
+++ serializers/resources.py
@@ -0,0 +1,55 @@
+from formula_one.serializers.base import ModelSerializer
+from Covid_Care.models import (
+    Resource,
+    Request,
+    Lead    
+)
+from Covid_Care.serializers import RequestsSerializer, LeadsSerializer
+from rest_framework import serializers
+
+
+class ResourceForTypeRelatedField(serializers):
+    """
+    
+    """
+
+    def to_representation(self, value):
+        """
+
+        """
+        if isinstance(value, Request):
+            serializer = RequestsSerializer(value)
+        elif isinstance(value, Lead):
+            serializer = LeadsSerializer(value)
+        else:
+            raise Exception('Unexpected Type of Field Provided.')
+        
+        return serializer.data
+
+
+class ResourceSerializer(ModelSerializer):
+    """
+    Details about the serializer
+    """
+    resource_for = ResourceForTypeRelatedField(read_only=True)
+
+    class Meta:
+        model = Lead
+        fields = [
+            'pk',
+            'name',
+            'uploader',
+            'contact',
+            'pin_code',
+            'upvotes',
+            'downvotes',
+            'address',
+            'verification',
+            'resource_for',
+            'other_contact',
+        ]
+        read_only = [
+            'name',
+            'pin_code',
+            'address',
+        ]
