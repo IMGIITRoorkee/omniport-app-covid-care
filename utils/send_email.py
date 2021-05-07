9f82321107727299436509e6803c6fccf1722b2e
Rochak Jain <rochak_r@ec.iitr.ac.in>
2021-05-07 15:27:55 +0530

    Handle errors for email_push function in email

--- utils/send_email.py
+++ utils/send_email.py
@@ -20,16 +20,19 @@ def send_request_form(person, pin_code):
     body_text = f'New request for region around {pin_code} is added.'
     target_app_url = f'https://channeli.in/'
 
-    email_push(
-        subject_text=f'New request added on Covid Care',
-        body_text=body_text,
-        category=category,
-        has_custom_user_target=True,
-        persons=person,
-        target_app_name=service.nomenclature.name,
-        target_app_url=target_app_url,
-        # send_only_to_subscribed_users=True
-    )
+    try:
+        email_push(
+            subject_text=f'New request added on Covid Care',
+            body_text=body_text,
+            category=category,
+            has_custom_user_target=True,
+            persons=person,
+            target_app_name=service.nomenclature.name,
+            target_app_url=target_app_url,
+            # send_only_to_subscribed_users=True
+        )
+    except Exception as e:
+        pass
 
 
 def send_lead_form(person, pin_code):
@@ -48,13 +51,16 @@ def send_lead_form(person, pin_code):
     body_text = f'New lead for region around {pin_code} is added.'
     target_app_url = f'https://channeli.in/'
 
-    email_push(
-        subject_text=f'New lead added on Covid Care',
-        body_text=body_text,
-        category=category,
-        has_custom_user_target=True,
-        persons=person,
-        target_app_name=service.nomenclature.name,
-        target_app_url=target_app_url,
-        # send_only_to_subscribed_users=True
-    )
+    try:
+        email_push(
+            subject_text=f'New lead added on Covid Care',
+            body_text=body_text,
+            category=category,
+            has_custom_user_target=True,
+            persons=person,
+            target_app_name=service.nomenclature.name,
+            target_app_url=target_app_url,
+            # send_only_to_subscribed_users=True
+        )
+    except Exception as e:
+        pass
