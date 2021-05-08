08c76c44b20418307cf559aee05851fbaffea13f
MihirSachdeva <mihirsachdeva.ms@gmail.com>
2021-05-08 13:38:29 +0530

    Check if lead or request exists in template view before generating template image

--- views/template_generate.py
+++ views/template_generate.py
@@ -1,8 +1,12 @@
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from covid_care.models import Request, Lead
 from covid_care.serializers import RequestsSerializer, LeadsSerializer
-from covid_care.utils.generate_card import store_request_template_image, store_lead_template_image
+from covid_care.utils.generate_card import (
+    store_request_template_image,
+    store_lead_template_image
+)
 
 
 class RequestTemplateImageView(APIView):
@@ -14,17 +18,27 @@ class RequestTemplateImageView(APIView):
     def get(self, request, *args, **kwargs):
         request_id = request.query_params.get('id', None)
         if request_id is not None:
+            try:
+                req = Request.objects.get(id=request_id)
+            except:
+                return Response(
+                    data='Request not found.',
+                    status=status.HTTP_404_NOT_FOUND
+                )
+
             request_template_link = store_request_template_image(request_id)
 
             return Response(
-                {'template_link': request_template_link},
+                data={
+                    'template_link': request_template_link
+                },
                 status=status.HTTP_200_OK
             )
         else:
             return Response(
-                data='Provide valid Request ID',
+                data='Request not found.',
                 status=status.HTTP_404_NOT_FOUND
-            )            
+            )
 
 
 class LeadTemplateImageView(APIView):
@@ -37,13 +51,22 @@ class LeadTemplateImageView(APIView):
         lead_id = request.query_params.get('id', None)
         if lead_id is not None:
             lead_template_link = store_lead_template_image(lead_id)
+            try:
+                lead = Request.objects.get(id=lead_id)
+            except:
+                return Response(
+                    data='Lead not found.',
+                    status=status.HTTP_404_NOT_FOUND
+                )
 
             return Response(
-                {'template_link': lead_template_link},
+                data={
+                    'template_link': lead_template_link
+                },
                 status=status.HTTP_200_OK
             )
         else:
             return Response(
-                data='Provide valid Lead ID',
+                data='Lead not found.',
                 status=status.HTTP_404_NOT_FOUND
             )
